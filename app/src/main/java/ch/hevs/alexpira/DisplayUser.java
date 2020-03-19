f195b02b43e92f2e6bacec0aafbd908896fea4d3
alex.gharbi@hotmail.com <alex.gharbi@hotmail.com>
2020-03-19 16:04:37 +0100

    in the process of fragment to activity

---
+++ app/src/main/java/ch/hevs/alexpira/DisplayUser.java
@@ -0,0 +1,14 @@
+package ch.hevs.alexpira;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DisplayUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_user);
+    }
+}
