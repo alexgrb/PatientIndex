2145a7612bd955e57aafc78deb44b8c9b839979a
alex.gharbi@hotmail.com <alex.gharbi@hotmail.com>
2020-03-20 10:25:37 +0100

    linked the activity to the main activity (there's an arrow permitting to go back to Main Activity)

--- app/src/main/java/ch/hevs/alexpira/AddUser.java
+++ app/src/main/java/ch/hevs/alexpira/AddUser.java
@@ -2,13 +2,55 @@ package ch.hevs.alexpira;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AddUser extends AppCompatActivity {
+    private Button submit;
+    private EditText firstName;
+    private EditText lastName;
+    private EditText address;
+    private EditText birthdate;
+    private EditText city;
+    private EditText npa;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_user);
+
+        submit = (Button) findViewById(R.id.btn_submit);
+        firstName = (EditText) findViewById(R.id.tv_firstName);
+        lastName = (EditText) findViewById(R.id.et_lastname);
+        address = (EditText) findViewById(R.id.et_address);
+        birthdate = (EditText) findViewById(R.id.et_birthdate);
+        city = (EditText) findViewById(R.id.et_city);
+        npa = (EditText) findViewById(R.id.et_NPA);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                /*
+                intent.putExtra("firstname", firstName.getText().toString());
+                intent.putExtra("lastname", lastName.getText().toString());
+                intent.putExtra("address", address.getText().toString());
+                intent.putExtra("city", city.getText().toString());
+                intent.putExtra("country", npa.getText().toString());
+                intent.putExtra("birthdate", birthdate.getText().toString());
+                 */
+                openActivity2();
+            }
+        });
+
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this, DisplayUser.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/ch/hevs/alexpira/DisplayUser.java
+++ app/src/main/java/ch/hevs/alexpira/DisplayUser.java
@@ -2,13 +2,41 @@ package ch.hevs.alexpira;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayUser extends AppCompatActivity {
+
+    private TextView firstName;
+    private TextView lastName;
+    private TextView address;
+    private TextView birthdate;
+    private TextView city;
+    private TextView npa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_user);
+
+        /*
+        Intent intent = getIntent();
+
+        firstName = (TextView) findViewById(R.id.tv_firstName);
+        lastName = (TextView) findViewById(R.id.et_lastname);
+        address = (TextView) findViewById(R.id.et_address);
+        city = (TextView) findViewById(R.id.et_city);
+        npa = (TextView) findViewById(R.id.et_NPA);
+        birthdate = (TextView) findViewById(R.id.et_birthdate);
+
+        firstName.setText(intent.getStringExtra("firstName"));
+        lastName.setText(intent.getStringExtra("lastName"));
+        address.setText(intent.getStringExtra("address"));
+        city.setText(intent.getStringExtra("city"));
+        npa.setText(intent.getStringExtra("NPA"));
+        birthdate.setText(intent.getStringExtra("birthdate"));
+
+         */
     }
 }
