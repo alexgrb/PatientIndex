c38ea35df1ce7ce6239d91ec66894e73dd902c2a
alex.gharbi@hotmail.com <alex.gharbi@hotmail.com>
2020-03-21 09:00:12 +0100

    Button "add bed" added + "edit bed added" with an intent of going back to AddBedActivity

--- app/src/main/java/ch/hevs/alexpira/DisplayBedActivity.java
+++ app/src/main/java/ch/hevs/alexpira/DisplayBedActivity.java
@@ -1,7 +1,11 @@
 package ch.hevs.alexpira;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class DisplayBedActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class DisplayBedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_bed);
+
+        Button buttonEdit = findViewById(R.id.button_edit);
+        buttonEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(DisplayBedActivity.this, AddBedActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
