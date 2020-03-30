33b45358360e40ab3057961f76ac0cb3d3e96e90
alex.gharbi@hotmail.com <alex.gharbi@hotmail.com>
2020-03-30 09:13:26 +0200

    ListBedActivity

---
+++ app/src/main/java/ch/hevs/alexpira/ui/bed/ListBedActivity.java
@@ -0,0 +1,16 @@
+package ch.hevs.alexpira.ui.bed;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ch.hevs.alexpira.R;
+
+public class ListBedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_bed);
+    }
+}
